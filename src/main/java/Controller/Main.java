dde2c798d88605b4f4ee941b29345a2343ee156d
Gurcenko <aleksandrs.gurcenko@gmail.com>
2020-03-29 21:04:16 +0100

    new look and feel

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -2,8 +2,23 @@ package Controller;
 
 import Model.AutoRefresh;
 
+import javax.swing.*;
+
 public class Main {
   public static void main(String[] args) {
+    try { //Set default theme to nimbus if installed
+      for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+          UIManager.setLookAndFeel(info.getClassName());
+          break;
+        }
+      }
+    } catch (Exception e) {
+      // If Nimbus is not available, you can set the GUI to the system look and feel.
+      try{
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      }catch (Exception all){}
+    }
     FolioTracker folioTracker=new FolioTracker();
     new AutoRefresh(folioTracker);
   }
